a343c2b52947180f3b3b3f914d82a66dab8b8dae
Vagan <arvaal@mail.ru>
2021-02-20 23:12:54 +0300

    Start framework

---
+++ application/core/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace application\core;
+
+class Router {
+    
+    public function __construct() {
+        echo "Класс роутер";
+    }
+    
+}
+
---
+++ application/lib/Dev.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+function debag(string $str) {
+    echo '<pre>';
+    var_dump($str);
+    echo '</pre>';
+    exit;
+}
